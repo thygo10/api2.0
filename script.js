7d562236e757af7a618fe8bce9b7531b415637ad
Thiago Farias <thygo10@icloud.com>
2023-06-21 20:33:28 -0300

    up

--- script.js
+++ script.js
@@ -8,7 +8,37 @@ const showData = (result) => {
     }
 }
 
-cep.addEventListener('blur', (e) => {
+const resetButton = document.querySelector('#reset')
+
+resetButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  cep.value = ''
+  document.querySelector('#logradouro').value = ''
+  document.querySelector('#complemento').value = ''
+  document.querySelector('#bairro').value = ''
+  document.querySelector('#localidade').value = ''
+  document.querySelector('#uf').value = ''
+  document.querySelector('#ibge').value = ''
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const submitButton = document.querySelector('#submit')
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
 let search = cep.value.replace('-', '')
 const options = {
     method: 'GET',
